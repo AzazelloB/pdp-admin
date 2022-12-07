0fce45e0109b71b7b001c712a546208313892990
Eduard <edwardak00@gmail.com>
2022-12-07 21:30:11 +0200

    create getUsers function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
 import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {getAuth} from "firebase-admin/auth";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const getUsers = functions
+    .region("europe-west2").https.onCall(
+        async (data) => {
+          const listUsersResult = await getAuth()
+              .listUsers(10, data?.pageToken);
+
+          return listUsersResult.users;
+        }
+    );
+
+export const grantUserRole = functions
+    .region("europe-west2").https.onCall(
+        async (data) => {
+          const user = await getAuth().getUserByEmail(data.email);
+
+          getAuth().setCustomUserClaims(user.uid, {
+            role: data.role,
+          });
+        }
+    );
+

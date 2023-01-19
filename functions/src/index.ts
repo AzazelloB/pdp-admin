3063364550acc8693a04059414318ba7f3dd6bab
Eduard <edwardak00@gmail.com>
2023-01-19 16:40:54 +0200

    fix cors error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,58 +2,72 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
 import {getAuth} from "firebase-admin/auth";
+import * as createCors from "cors";
 
 initializeApp();
 const db = getFirestore();
 
-export const getUserRoleList = functions
-    .region("europe-west2").https.onCall(
-        async () => {
-          const authenticatedUsersResult = await getAuth()
-              .listUsers();
-          const userRoleListResult = await db.collection("userRole").get();
+const cors = createCors({origin: true});
 
-          return [
-            ...authenticatedUsersResult.users.map((user) => ({
-              email: user.email,
-              role: user.customClaims?.role,
-            })),
-            ...userRoleListResult.docs.map((doc) => ({
-              email: doc.get("email"),
-              role: doc.get("role"),
-            })),
-          ];
-        }
-    );
+type Handler = (
+  req: functions.https.Request,
+  resp: functions.Response<unknown>
+) => Promise<unknown>;
 
-export const setUserRole = functions
-    .region("europe-west2").https.onCall(
-        async (data) => {
-          try {
-            const user = await getAuth().getUserByEmail(data.email);
+const createRequest = (handler: Handler) => {
+  return functions.region("europe-west2").https.onRequest((req, res) => {
+    cors(req, res, async () => {
+      const result = await handler(req, res);
 
-            getAuth().setCustomUserClaims(user.uid, {
-              role: data.role,
-            });
-          } catch {
-            const docs = await db
-                .collection("userRole")
-                .where("email", "==", data.email)
-                .get();
+      res.send({result});
+    });
+  });
+};
 
-            if (docs.docs[0]?.id) {
-              db.collection("userRole").doc(docs.docs[0].id).update({
-                role: data.role,
-              });
-            }
-          }
+export const getUserRoleList = createRequest(async () => {
+  const authenticatedUsersResult = await getAuth().listUsers();
+  const userRoleListResult = await db.collection("userRole").get();
 
-          return {
-            email: data.email,
-            role: data.role,
-          };
-        }
-    );
+  return [
+    ...authenticatedUsersResult.users.map((user) => ({
+      email: user.email,
+      role: user.customClaims?.role,
+    })),
+    ...userRoleListResult.docs.map((doc) => ({
+      email: doc.get("email"),
+      role: doc.get("role"),
+    })),
+  ];
+});
+
+export const setUserRole = createRequest(async (req) => {
+  const data = JSON.parse(req.body);
+
+  try {
+    const user = await getAuth().getUserByEmail(data.email);
+
+    getAuth().setCustomUserClaims(user.uid, {
+      role: data.role,
+    });
+  } catch {
+    const docs = await db
+        .collection("userRole")
+        .where("email", "==", data.email)
+        .get();
+
+    if (docs.docs[0]?.id) {
+      db.collection("userRole").doc(docs.docs[0].id).update({
+        role: data.role,
+      });
+    }
+  }
+
+  return {
+    email: data.email,
+    role: data.role,
+  };
+}
+);
 
 export const addUserRole = functions
     .region("europe-west2").https.onCall(

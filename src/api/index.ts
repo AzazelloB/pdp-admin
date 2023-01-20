ac1eea24874eeaf507e44b790e80d377275b3562
Eduard <edwardak00@gmail.com>
2023-01-20 15:15:24 +0200

    add prod env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,23 +1,21 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-export const createAxiosInstance = (config: AxiosRequestConfig) => {
+export const createAxiosInstance = (config: Partial<AxiosRequestConfig>) => {
   const newInstance = axios.create({
+    ...config,
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     headers: {
       'Content-Type': 'application/json',
       ...config.headers,
     },
-    ...config,
   });
 
   return newInstance;
 };
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 const api = createAxiosInstance({
-  baseURL: 'http://localhost:5001/review-form-e7e13/europe-west2/',
+  baseURL: process.env.REACT_APP_API_BASE,
 });
 
 export {

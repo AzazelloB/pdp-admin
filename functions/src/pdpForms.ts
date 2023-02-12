3cc0c0a74941e7222e2f653b3a830aa6a03d4d5b
Eduard <edwardak00@gmail.com>
2023-02-12 18:05:36 +0200

    fetch template with pdp form

--- functions/src/pdpForms.ts
+++ functions/src/pdpForms.ts
@@ -2,7 +2,7 @@ import { db } from './utils/bootstrap';
 import { isAuthenticated } from './utils/guards';
 import { createRequest } from './utils/request';
 
-export const getUserForm = createRequest(async (req, res) => {
+export const getUserPDPForm = createRequest(async (req, res) => {
   const id = req.params[0];
 
   const ref = await db
@@ -20,5 +20,25 @@ export const getUserForm = createRequest(async (req, res) => {
     return res.status(403).send();
   }
 
-  return PDPForm;
+  const template = await PDPForm.template.get();
+
+  return {
+    ...PDPForm,
+    template: template.data(),
+  };
+}, [isAuthenticated]);
+
+export const getUserPDPTemplate = createRequest(async (req, res) => {
+  const id = req.params[0];
+
+  const ref = await db
+    .collection('formTemplates')
+    .doc(id)
+    .get();
+
+  if (!ref.exists) {
+    return res.status(404).send();
+  }
+
+  return ref.data()!;
 }, [isAuthenticated]);
